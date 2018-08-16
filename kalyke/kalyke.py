4ed3404daccf967a6f0df54997c243a9bf3602ca
Barry Smyth <barry.smyth@ucd.ie>
2018-08-16 12:39:56 +0100

    Added new styles submodule for seaborn related line styles.

--- kalyke/kalyke.py
+++ kalyke/kalyke.py
@@ -2,15 +2,4 @@
 
 """Main module."""
 
-RED_LINE_SPEC = {
-    'scatter_kws': {'color':'coral', 's':200, 'alpha': 0.5},
-    'line_kws': {'color': 'coral', 'lw': 2}}
-BLUE_LINE_SPEC = {
-    'scatter_kws': {'color':'cornflowerblue', 's':200, 'alpha': 0.5},
-    'line_kws': {'color': 'cornflowerblue', 'lw': 2}}
-GREEN_LINE_SPEC = {
-    'scatter_kws': {'color':'green', 's':200, 'alpha': 0.5},
-    'line_kws': {'color': 'green', 'lw': 2}}
-BLACK_LINE_SPEC = {
-    'scatter_kws': {'color':'k', 's':120000, 'alpha': 0.5},
-    'line_kws': {'color': 'k', 'lw': 2, 'ls': '--'}}
+
